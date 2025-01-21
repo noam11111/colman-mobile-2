3ba6e23b33ba49e3446815030795539b9a1f5f9a
Noam Ifat <noamifat@gmail.com>
2025-01-21 20:31:25 +0200

    fix bug in adding

--- app/src/main/java/com/example/studentsapp/Model.kt
+++ app/src/main/java/com/example/studentsapp/Model.kt
@@ -9,7 +9,7 @@ class Model private constructor() {
     }
 
     init {
-        for (i in 0..20) {
+        for (i in 0..3) {
             val student = Student(
                 name = "noam$i",
                 id = "$i",
